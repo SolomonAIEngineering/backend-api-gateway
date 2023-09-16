65edafae20de36b09f6ff149fe509ccb6532acb1
yoan <yoanyombapro@gmail.com>
2023-09-16 02:18:47 -0400

    feat: add api-gateway repo
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- scripts/bootstrap-flux.sh
+++ scripts/bootstrap-flux.sh
@@ -8,6 +8,31 @@ export GITHUB_USER=SolomonAIEngineering
 # check kubernetes cluster
 flux check --pre
 
+
+refine_repo_and_kustomize() {
+   local kubernetes_ctx=$1
+   local environment=$2
+
+    flux create source git api-gateway \  
+        --context=$kubernetes_ctx \                    
+        --url=https://git@github.com/SolomonAIEngineering/backend-api-gateway \
+        --branch=main \
+        --interval=1m \
+        --export > ./clusters/$environment/service-source.yaml
+    
+    flux create kustomization api-gateway \
+        --context=$kubernetes_ctx \                    
+        --target-namespace=platform \
+        --source=api-gateway \
+        --path="./kustomize/overlays/$environment" \
+        --prune=true \
+        --wait=true \
+        --interval=30m \
+        --retry-interval=2m \
+        --health-check-timeout=3m \
+        --export > ./clusters/$environment/service-kustomization.yaml
+}
+
 # Define a function to bootstrap flux for a given environment.
 bootstrap_flux() {
   local kubernetes_ctx=$1
@@ -20,7 +45,10 @@ bootstrap_flux() {
     --branch=main \
     --path=./clusters/$environment \
     --personal
+
+    refine_repo_and_kustomize "${kubernetes_ctx}" "${environment}"
 }
+
 
 # Use the function to install flux onto the staging cluster.
 bootstrap_flux "do-nyc3-solomon-ai-k8s-staging" "staging"
