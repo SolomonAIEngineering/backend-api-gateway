5d4eb986b87cb351d38982dc45d94f9c15ecf82a
yoanyombapro1234 <yoanyombapro@gmail.com>
2023-11-04 23:51:52 -0700

    feat: update docs
    Signed-off-by: yoanyombapro1234 <yoanyombapro@gmail.com>

--- docs/.output/public/_nuxt/ComponentPlaygroundSlots.739c724d.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./ComponentPlaygroundSlots.vue.8d324064.js";import"./entry.3845ec98.js";export{m as default};
--- docs/.output/public/_nuxt/EditOnLink.fb9c496c.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./EditOnLink.vue.32101e85.js";import"./entry.3845ec98.js";export{m as default};
