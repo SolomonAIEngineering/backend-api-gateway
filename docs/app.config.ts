d1718e2b1e7f1db51d3514084fa6987a9820d7ab
Lens <45818877+yoanyombapro1234@users.noreply.github.com>
2023-11-06 05:36:32 -0500

    feat: update kustomize file with proper configs (#12)
    * feat: update kustomize file with proper configs
    
    Signed-off-by: yoanyombapro1234 <yoanyombapro@gmail.com>
    
    * feat: onboard to v2 of authorization and user profile
    
    Signed-off-by: yoan <yoanyombapro@gmail.com>
    
    * feat: onboard to v2 of authorization and user profile
    
    Signed-off-by: yoan <yoanyombapro@gmail.com>
    
    * feat: onboard to v2 of authorization and user profile
    
    Signed-off-by: yoan <yoanyombapro@gmail.com>
    
    * feat: onboard to v2 of authorization and user profile
    
    Signed-off-by: yoan <yoanyombapro@gmail.com>
    
    * feat: onboard to v2 of authorization and user profile
    
    Signed-off-by: yoan <yoanyombapro@gmail.com>
    
    * feat: onboard to v2 of authorization and user profile
    
    Signed-off-by: yoan <yoanyombapro@gmail.com>
    
    * feat: update nuxt config
    
    Signed-off-by: yoanyombapro1234 <yoanyombapro@gmail.com>
    
    * feat: update docs
    
    Signed-off-by: yoanyombapro1234 <yoanyombapro@gmail.com>
    
    * feat: update docs site
    
    Signed-off-by: yoanyombapro1234 <yoanyombapro@gmail.com>
    
    * feat: update docs
    
    Signed-off-by: yoanyombapro1234 <yoanyombapro@gmail.com>
    
    ---------
    
    Signed-off-by: yoanyombapro1234 <yoanyombapro@gmail.com>
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   docus: {
-    title: 'Melodiy API Gateway',
-    description: 'API Gateway service for the melodiy backend based off of krakend',
+    title: 'Docus',
+    description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       twitter: 'nuxt_js',
